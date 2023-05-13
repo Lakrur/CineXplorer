6867356b560ed0f8ef83dd324906bab31f4a518c
Yehor Krupiei <yehorkrupiei@gmail.com>
2023-05-13 16:07:21 +0300

    Initial Commit

---
+++ CineXplorer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CineXplorer
+//
+//  Created by Yehor Krupiei on 13.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
